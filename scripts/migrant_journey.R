7e875c5ad54f31238292a21c096e918fa7214670
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2022-07-10 15:28:28 +0100

    a bit of tidying

--- scripts/migrant_journey.R
+++ scripts/migrant_journey.R
@@ -1,18 +1,22 @@
 
 # migrant journey data
 # from the Home Office
-# entry to UK by visa category
-# and expiry year
+# entry to UK by visa category and expiry year
+# estimate survival curves
 
 library(readr)
 library(dplyr)
+library(purrr)
+
 
 migrant_data <- read_csv("data/migrant_journey_updatedSep21_e_data.csv")
 
-migrant_data$valid_visa <- c(migrant_data$value[-1],NA)
-migrant_data$total <- migrant_data$value + migrant_data$valid_visa
-migrant_data$prop <- migrant_data$value/migrant_data$total
-migrant_data <- migrant_data[migrant_data$status_year_end == "Expired", ]
+migrant_data <-
+  migrant_data |>
+  mutate(valid_visa = c(migrant_data$value[-1], NA),
+         total = value + valid_visa,
+         prop = value/total) |>
+  filter(status_year_end == "Expired")
 
 # initial population entering UK
 migrant_pop <-
@@ -47,7 +51,7 @@ sample_dat <-
 jags_dat <-
   list(N = sample_dat$total,
        x = sample_dat$value,
-       nat = as.numeric(as.factor(sample_dat$nationality)),  # Bangladesh China India Pakistan Sub_Saharan_Africa
+       nat = as.numeric(as.factor(sample_dat$nationality)),  # Bangladesh, China, India, Pakistan, Sub_Saharan_Africa
        id = as.numeric(as.factor(sample_dat$year_issued)),
        t = sample_dat$year - min(sample_dat$year) + 1,
        n_id = length(unique(sample_dat$year_issued)),
@@ -57,6 +61,9 @@ jags_dat <-
 # params <- c("rate", "pi")
 # params <- c("mu", "sigma", "pi", "S_pred", "x_pred")
 params <- c("mu", "sigma", "pi")
+
+n_nat <- 5
+n_yr <- 16
 
 n.iter <- 10000
 n.burnin <- 5000
@@ -68,9 +75,9 @@ out <-
   jags(data = jags_dat,
        parameters.to.save = params,
        model.file = "scripts/BUGS_script.txt",
-       inits = list(list("mu" = matrix(0,17,5),
-                         "sigma" = matrix(1,17,5),
-                         "pi" = matrix(0.5,17,5))),
+       inits = list(list("mu" = matrix(0,17, n_nat),
+                         "sigma" = matrix(1,17, n_nat),
+                         "pi" = matrix(0.5,17, n_nat))),
        # inits = list(list("rate" = 0.5, "pi" = 0.5)),
        n.chains = 1,
        n.iter,
@@ -88,7 +95,9 @@ save(out, file = "data/BUGS_output.RData")
 # plots
 ########
 
-library(purrr)
+library(reshape2)
+library(ggplot2)
+
 
 props <- split(sample_dat$S, jags_dat$id)
 
@@ -96,16 +105,17 @@ mean_mu <- apply(out$BUGSoutput$sims.list$mu, FUN = mean, MARGIN = c(2,3))
 mean_pi <- apply(out$BUGSoutput$sims.list$pi, FUN = mean, MARGIN = c(2,3))
 mean_sigma <- apply(out$BUGSoutput$sims.list$sigma, FUN = mean, MARGIN = c(2,3))
 
-plot(mean_pi[5,5] + (1-mean_pi[5,5])*plnorm(q = seq(0,17,1), meanlog = mean_mu[5,5], sdlog = mean_sigma[5,5], lower.tail = FALSE),
+plot(mean_pi[5,5] + (1-mean_pi[5,5])*plnorm(
+  q = seq(0,17,1), meanlog = mean_mu[5,5], sdlog = mean_sigma[5,5], lower.tail = FALSE),
      ylim = c(0,1), type = "l")
 
-res <- array(dim = c(16,18,5),
+res <- array(dim = c(n_yr, 18, n_nat),
              dimnames = list(year_issue = 2004:2019,
                              year_expire = 2003:2020,
                              nat = unique(sample_dat$nationality)))
 
-for (j in 1:5) {
-  for (i in 1:16) {
+for (j in seq_len(n_nat)) {
+  for (i in seq_len(n_yr)) {
     cf <- mean_pi[i,j]
     S_mean <- cf + (1-cf)*plnorm(q = seq(0,17,1),
                                  meanlog = mean_mu[i,j],
@@ -119,22 +129,20 @@ for (j in 1:5) {
 
 # empirical
 plot(NA, ylim = c(0,1), xlim = c(0,18))
-map(1:17, ~lines(jags_dat$t[jags_dat$id == .] + 1, props[[.]], ylim = c(0,1), type = "o"))
+map(1:(n_yr+1), ~lines(jags_dat$t[jags_dat$id == .] + 1, props[[.]], ylim = c(0,1), type = "o"))
 
 for (i in 1:2000) {
-  lines(pi[i] + (1-pi[i])*plnorm(q = seq(0,17,1), meanlog = mu[i], sdlog = sigma[i], lower.tail = FALSE), col = "lightgrey")
+  lines(pi[i] + (1-pi[i])*plnorm(
+    q = seq(0, (n_yr+1), 1), meanlog = mu[i], sdlog = sigma[i], lower.tail = FALSE), col = "lightgrey")
 }
 
+## expire survival tidy data
 
-library(reshape2)
-library(ggplot2)
-library(dplyr)
-
-xx <-
+res_long <-
   melt(res) |>
   mutate(grp = paste(year_issue, nat))
 
-ggplot(xx, aes(x = year_expire, y = value, group = grp, col = nat)) + geom_line() + ylim(0,1) +
+ggplot(res_long, aes(x = year_expire, y = value, group = grp, col = nat)) + geom_line() + ylim(0,1) +
   # facet_wrap(vars(nat))
   facet_wrap(vars(year_issue))
 
@@ -143,7 +151,7 @@ ggplot(xx, aes(x = year_expire, y = value, group = grp, col = nat)) + geom_line(
 
 
 # save survival fits
-save(res, file = "data/posterior_S.RData")
+save(res_long, file = "data/posterior_S.RData")
 
 
 
